ba1d97f54c8621eb173186aaeb24f033d066e89a
Amogh2513 <amoghjainapur@gmail.com>
2024-04-30 10:36:25 +0530

    added

--- data.java
+++ data.java
@@ -3,8 +3,11 @@ public class database {
 	public static void main(String[] args) {
 		System.out.println("hello welcome to gpt");
 		System.out.println("hello welcome to gpt");
+        System.out.println("hello welcome to gpt");
+        System.out.println("hello welcome to gpt");
+
 
 
 	}
 
-}
+} 
