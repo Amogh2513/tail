456860ae14a0c2397a137e090683774490fda791
Amogh2513 <amoghjainapur@gmail.com>
2024-04-30 11:15:38 +0530

    update

--- data.java
+++ data.java
@@ -2,6 +2,8 @@ public class database {
 
 	public static void main(String[] args) {
 		System.out.println("hello welcome to gpt");
+        System.out.println("hello welcome to gpt");
+        System.out.println("hello welcome to hello");
 
 	}
 
